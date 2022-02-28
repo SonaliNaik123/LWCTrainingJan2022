93f41bc89c13e244b34f683226c4438fc64f788d
SonaliNaik123 <98101377+SonaliNaik123@users.noreply.github.com>
2022-02-28 10:55:43 +0530

    childTwo.js

---
+++ Module5/childTwo.js
@@ -0,0 +1,31 @@
+import { LightningElement,api } from 'lwc';
+
+export default class ChildTwo extends LightningElement {
+    @api btnlabel = "Select";
+    btnStyle = "success";
+    @api childName = "ChildTwo";
+
+    @api childReset(){
+        this.btnlabel = "Select";
+        this.btnStyle = "success";
+    }
+
+    handleClick(event){
+        const event1 = new CustomEvent('childclick',
+        {
+            bubbles: true,
+            composed: true,
+            detail: this.btnlabel
+        });
+        this.dispatchEvent(event1);
+
+        if (this.btnlabel === "Select"){
+            this.btnlabel = "Deselect";
+            this.btnStyle = "destructive";
+        }
+        else{
+            this.btnlabel = "Select";
+            this.btnStyle = "success";
+        }
+    }
+}
